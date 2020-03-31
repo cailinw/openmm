a4013692f26834b8848efc26d5ad24aa89c2b4a7
Cailin <cailinwin@gmail.com>
2020-03-31 00:04:47 -0700

    adding skeleton code for kernels

--- platforms/emu/include/EmuKernels.h
+++ platforms/emu/include/EmuKernels.h
@@ -10,12 +10,57 @@ namespace OpenMM {
 
 
 /**
+ * This kernel is invoked at the beginning and end of force and energy computations.  It gives the
+ * Platform a chance to clear buffers and do other initialization at the beginning, and to do any
+ * necessary work at the end to determine the final results.
+ */
+class EmuCalcForcesAndEnergyKernel : public CalcForcesAndEnergyKernel {
+public:
+    EmuCalcForcesAndEnergyKernel(std::string name, const Platform& platform) : CalcForcesAndEnergyKernel(name, platform) {
+    }
+    /**
+     * Initialize the kernel.
+     * 
+     * @param system     the System this kernel will be applied to
+     */
+    void initialize(const System& system);
+    /**
+     * This is called at the beginning of each force/energy computation, before calcForcesAndEnergy() has been called on
+     * any ForceImpl.
+     *
+     * @param context       the context in which to execute this kernel
+     * @param includeForce  true if forces should be computed
+     * @param includeEnergy true if potential energy should be computed
+     * @param groups        a set of bit flags for which force groups to include
+     */
+    void beginComputation(ContextImpl& context, bool includeForce, bool includeEnergy, int groups);
+    /**
+     * This is called at the end of each force/energy computation, after calcForcesAndEnergy() has been called on
+     * every ForceImpl.
+     *
+     * @param context       the context in which to execute this kernel
+     * @param includeForce  true if forces should be computed
+     * @param includeEnergy true if potential energy should be computed
+     * @param groups        a set of bit flags for which force groups to include
+     * @param valid         the method may set this to false to indicate the results are invalid and the force/energy
+     *                      calculation should be repeated
+     * @return the potential energy of the system.  This value is added to all values returned by ForceImpls'
+     * calcForcesAndEnergy() methods.  That is, each force kernel may <i>either</i> return its contribution to the
+     * energy directly, <i>or</i> add it to an internal buffer so that it will be included here.
+     */
+    double finishComputation(ContextImpl& context, bool includeForce, bool includeEnergy, int groups, bool& valid);
+private:
+    std::vector<Vec3> savedForces;
+};
+
+
+/**
  * This kernel provides methods for setting and retrieving various state data: time, positions,
  * velocities, and forces.
  */
 class EmuUpdateStateDataKernel : public UpdateStateDataKernel {
 public:
-    EmuUpdateStateDataKernel(std::string name, const Platform& platform, int contextId) : UpdateStateDataKernel(name, platform), contextId(contextId) {
+    EmuUpdateStateDataKernel(std::string name, const Platform& platform, EmuPlatform::PlatformData& data) : UpdateStateDataKernel(name, platform), data(data) {
     }
     /**
      * Initialize the kernel.
@@ -100,9 +145,65 @@ public:
      */
     void loadCheckpoint(ContextImpl& context, std::istream& stream);
 private:
-    int contextId;
+    EmuPlatform::PlatformData& data;
 };
 
+
+/**
+ * This kernel modifies the positions of particles to enforce distance constraints.
+ */
+class EmuApplyConstraintsKernel : public ApplyConstraintsKernel {
+public:
+    EmuApplyConstraintsKernel(std::string name, const Platform& platform, EmuPlatform::PlatformData& data) :
+            ApplyConstraintsKernel(name, platform), data(data) {
+    }
+    ~EmuApplyConstraintsKernel();
+    /**
+     * Initialize the kernel.
+     *
+     * @param system     the System this kernel will be applied to
+     */
+    void initialize(const System& system);
+    /**
+     * Update particle positions to enforce constraints.
+     *
+     * @param context    the context in which to execute this kernel
+     * @param tol        the distance tolerance within which constraints must be satisfied.
+     */
+    void apply(ContextImpl& context, double tol);
+    /**
+     * Update particle velocities to enforce constraints.
+     *
+     * @param context    the context in which to execute this kernel
+     * @param tol        the velocity tolerance within which constraints must be satisfied.
+     */
+    void applyToVelocities(ContextImpl& context, double tol);
+private:
+    EmuPlatform::PlatformData& data;
+    std::vector<double> masses;
+    std::vector<double> inverseMasses;
+};
+
+/**
+ * This kernel recomputes the positions of virtual sites.
+ */
+class EmuVirtualSitesKernel : public VirtualSitesKernel {
+public:
+    EmuVirtualSitesKernel(std::string name, const Platform& platform) : VirtualSitesKernel(name, platform) {
+    }
+    /**
+     * Initialize the kernel.
+     *
+     * @param system     the System this kernel will be applied to
+     */
+    void initialize(const System& system);
+    /**
+     * Compute the virtual site locations.
+     *
+     * @param context    the context in which to execute this kernel
+     */
+    void computePositions(ContextImpl& context);
+};
 
 } // namespace OpenMM
 #endif /*OPENMM_EMUKERNELS_H_*/
